da17187ff2c4fd9959be10582de4638235aa7fa8
Daniel Gray <dng@disroot.org>
2019-04-05 18:35:08 +0000

    Don't have many extensions installed, prefer these to update

--- user.js
+++ user.js
@@ -187,7 +187,7 @@ user_pref("_user.js.parrot", "0300 syntax error: the parrot's not pinin' for the
 user_pref("app.update.auto", false);
 /* 0302b: disable auto-INSTALLING extension and theme updates (after the check in 0301b)
  * [SETTING] about:addons>Extensions>[cog-wheel-icon]>Update Add-ons Automatically (toggle) ***/
-user_pref("extensions.update.autoUpdateDefault", false);
+user_pref("extensions.update.autoUpdateDefault", true);
 /* 0303: disable background update service [WINDOWS]
  * [SETTING] General>Firefox Updates>Use a background service to install updates ***/
 user_pref("app.update.service.enabled", false);
